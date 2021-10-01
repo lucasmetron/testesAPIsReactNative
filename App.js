6a392da21b43298fd34212259436559825209e86
Lucas Rosa <lucasmetron@gmail.com>
2021-10-01 08:45:23 -0300

    sensores

--- App.js
+++ App.js
@@ -6,56 +6,26 @@ import {
   Button,
   Image,
 } from 'react-native';
-
-import Geolocation from 'react-native-geolocation-service'
+import {
+  accelerometer,
+  gyroscope,
+  magnetometer,
+  barometer,
+  setUpdateIntervalForType,
+  SensorTypes
+} from 'react-native-sensors'
 
 const App = () => {
-
-  const hasPermission = async () => {
-    const openSetting = () => {
-      Linking.openSettings().catch(() => {
-        Alert.alert('Unable to open settings');
-      });
-    };
-    const status = await Geolocation.requestAuthorization('whenInUse');
-
-    if (status === 'granted') {
-      return true;
-    }
-
-    if (status === 'denied') {
-      Alert.alert('Location permission denied');
-    }
-
-    if (status === 'disabled') {
-      Alert.alert(
-        `Turn on Location Services to allow "${appConfig.displayName}" to determine your location.`,
-        '',
-        [
-          { text: 'Go to Settings', onPress: openSetting },
-          { text: "Don't Use Location", onPress: () => { } },
-        ],
-      );
-    }
-
-    return false;
-  };
-
-
   useEffect(() => {
-    console.log('teste')
-    // Geolocation.getCurrentPosition(
-    //   (position) => {
-    //     console.log('posição', position);
-    //   },
-    //   (error) => {
-    //     // See error code charts below.
-    //     console.log('erro', error.code, error.message);
-    //   },
-    //   { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 })
+    accelerometer.subscribe(({ x, y, z }) => {
+      console.log(x, y, z);
+    })
 
-    const watchID = Geolocation.watchPosition(res => console.log(res))
-    // console.log(watchID)
+    barometer.subscribe(({ pressure }) => {
+      console.log(pressure)
+    })
+
+    setUpdateIntervalForType(SensorTypes.barometer, 1000); //nesta função podemos escolher qual sensor queremos mudar o tempo de leitura
 
   }, [])
 
@@ -64,6 +34,7 @@ const App = () => {
   return (
     <SafeAreaView>
       <View>
+        <Text>OI</Text>
         <Text>OI</Text>
 
       </View>

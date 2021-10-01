110429d0c2cfb65ceddf29350d44690d632dc2e7
Lucas Rosa <lucasmetron@gmail.com>
2021-10-01 08:29:32 -0300

    geolozalização

--- App.js
+++ App.js
@@ -7,82 +7,65 @@ import {
   Image,
 } from 'react-native';
 
-import CameraRoll from '@react-native-community/cameraroll'
-import { RNCamera } from 'react-native-camera'
-import { launchImageLibrary, launchCamera } from 'react-native-image-picker'
-
+import Geolocation from 'react-native-geolocation-service'
 
 const App = () => {
 
-  let [camera, setCamera] = useState(''); //armazena ref da camera
-  const [uri, setUri] = useState('') //endereço da imagem salva
+  const hasPermission = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow "${appConfig.displayName}" to determine your location.`,
+        '',
+        [
+          { text: 'Go to Settings', onPress: openSetting },
+          { text: "Don't Use Location", onPress: () => { } },
+        ],
+      );
+    }
+
+    return false;
+  };
+
 
   useEffect(() => {
-    // async function get() {
-    //   let response = await CameraRoll.getPhotos({
-    //     first: 10,
-    //     assetType: 'Photos',
+    console.log('teste')
+    // Geolocation.getCurrentPosition(
+    //   (position) => {
+    //     console.log('posição', position);
+    //   },
+    //   (error) => {
+    //     // See error code charts below.
+    //     console.log('erro', error.code, error.message);
+    //   },
+    //   { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 })
 
-    //   })
-    //   console.log('responde', response)
-    // }
-    // get();
-
-    launchCamera({}, async (response) => { //abre camera para tirar foto
-      if (response.uri) {
-        const blob = await (await fetch(response.uri)).blob();
-        currentStorageRef.child(response.fileName).put(blob, { contentType: response.type })
-        resolve(currentStorageRef);
-      } else {
-        reject();
-      }
-    })
-
-    launchImageLibrary({}, async (response) => { //abre galeria
-      if (response.uri) {
-        const blob = await (await fetch(response.uri)).blob();
-        currentStorageRef.child(response.fileName).put(blob, { contentType: response.type })
-        resolve(currentStorageRef);
-      } else {
-        reject();
-      }
-    })
-
+    const watchID = Geolocation.watchPosition(res => console.log(res))
+    // console.log(watchID)
 
   }, [])
 
-
-  // async function shot() {
-  //   console.log('teste')
-  //   if (camera) {
-  //     const options = { quality: 0.5, base64: true }
-  //     const data = await camera.takePictureAsync(options)
-  //     setUri(data.uri)
-  //     console.log(data)
-  //     await CameraRoll.save(data.uri, { type: 'photo' })
-  //     get()
-  //   }
-
-  // }
 
 
   return (
     <SafeAreaView>
       <View>
         <Text>OI</Text>
-        <Text>OI</Text>
-        <Text>OI</Text>
-        {/* <RNCamera
-          ref={ref => { setCamera(ref) }} //ref da camera
-          style={{ margin: 50, height: 200, width: 200 }}
-          type={RNCamera.Constants.Type.front} //setando camera frontal
-          flashMode={RNCamera.Constants.FlashMode.on} //ligando o flah
-        /> */}
-        {/* <Button
-          title="Tirar Fotooo"
-          onPress={shot}
-        /> */}
-        <Image style={{ width: 100, height: 100 }} source={{ uri: uri }} />
+
       </View>
     </SafeAreaView>
   );
